5f2b942c6fc7e1b3d77d0c22aeec2e1f23a282c6
Anton Sakhanovych <dev@antonsakhanovych.com>
2025-10-03 10:25:40 -0400

    food search takes 1 positional argument(query)

--- usda/food-search.sh
+++ usda/food-search.sh
@@ -1,1 +1,10 @@
-curl -X POST "https://api.nal.usda.gov/fdc/v1/foods/search?api_key=$USDA_FDC_API_KEY" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{\"query\":\"Abiyuch\",\"dataType\":[\"Foundation\",\"SR Legacy\"],\"pageSize\":25,\"pageNumber\":2,\"sortBy\":\"dataType.keyword\",\"sortOrder\":\"asc\",\"brandOwner\":\"Kar Nut Products Company\"}"
+#!/bin/sh
+
+if [ -z $1 ]; then
+    echo "Please provide a query"
+    exit 1
+fi
+
+QUERY=$1
+
+curl -X POST "https://api.nal.usda.gov/fdc/v1/foods/search?api_key=$USDA_FDC_API_KEY" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{\"query\":\"$QUERY\",\"pageSize\":25,\"pageNumber\":2,\"sortBy\":\"dataType.keyword\",\"sortOrder\":\"asc\"}"
